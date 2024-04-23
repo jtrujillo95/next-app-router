bda24376891f53b0596df38ba674e71274c1c0c7
Jorge Trujillo <jtrujillo95@outlook.com>
2024-04-23 16:29:32 -0600

    Chapter 16: Adding Metadata

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,14 @@
 import { Suspense } from 'react';
-// import { Card } from '@/app/ui/dashboard/cards';
 import CardWrapper from '../ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-// import { fetchCardData } from '../lib/data';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '../ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard'
+}
  
 export default async function Page() {
 
@@ -18,14 +21,6 @@ export default async function Page() {
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
